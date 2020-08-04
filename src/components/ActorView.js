f75a26b249c7fceb7fb00f3e90cd0f64c88ab79e
MeriSg <sgmerisg@gmail.com>
2020-08-04 14:26:54 +0300

    add input filter

--- src/components/ActorView.js
+++ src/components/ActorView.js
@@ -5,32 +5,44 @@ class ActorView extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            name: ""    
+        }
     }
 
+    updateName = (event) =>{
+        this.setState({
+          name: event.target.value
+        });
+    }
 
     render() {
 
-        let contentToRender;
-       
-        contentToRender= this.props.actor.map( actor =>
+        const actor = this.props.actor;
 
-            <div class="col-xl-4">
-                <div class="card">
-                    <img class="card-img-top" src={actor.imageUrl} alt="Card image"/>
-                    <div class="card-body">
-                        <a target="void" href={actor.imdbLink}> <h4 class="card-title">{actor.firstName} {actor.lastName}</h4> </a>
-                        <p class="card-text">Birthday: {actor.birthday} (age:{actor.findAge()}) </p>
-                        <a target="void" href={actor.imdbLink} class="btn btn-primary">See IMDB Profile</a>
+        let contentToRender= actor.filter(actor => (actor.firstName+" "+actor.lastName).toLowerCase().includes((this.state.name).toLowerCase())).map(filterActor =>
+            <div key={filterActor.imdbLink} className="col-xl-4">
+                <div className="card">
+                    <img className="card-img-top" src={filterActor.imageUrl} alt={filterActor.firstName}/>
+                    <div className="card-body">
+                        <a target="void" href={filterActor.imdbLink}> <h4 className="card-title">{filterActor.firstName} {filterActor.lastName}</h4> </a>
+                        <p className="card-text">Birthday: {filterActor.birthday} (age:{filterActor.findAge()}) </p>
+                        <a target="void" href={filterActor.imdbLink} className="btn btn-primary">See IMDB Profile</a>
                     </div>
                 </div>
-            </div> ); 
+            </div>); 
 
         return (
-            <Container>
-                <div class="row">
-                    {contentToRender}
-                </div>
-            </Container>        
+            <div className="wrap">
+                <input type="text" id="name" placeholder="Enter a name" onChange={this.updateName} value={this.state.name}/>
+
+                <Container>
+                    <div className="row">
+                        {contentToRender}
+                    </div>
+                </Container> 
+            </div>
+               
         );
     }
 }
